2e19a5fd01942dc89ef9c9f47c4945cea4f86b4c
Wendy Liang <wendyl.y.123@gmail.com>
2024-06-19 23:21:33 -0700

    update navigation to not use post method

--- indexscript.js
+++ indexscript.js
@@ -4,18 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
   const usernameInput = document.getElementById('username');
 
   form.addEventListener('submit', (event) => {
+      event.preventDefault();
       const usernameValue = usernameInput.value;
       const requiredUsername = 'sillygoose';
       if (usernameValue !== requiredUsername) {
-        event.preventDefault();
         alert('Incorrect username bb gurl. Please try again.');
       }      
       const passwordValue = passwordInput.value;
       const requiredPassword = '06191997';
-      console.log(passwordValue);
       if (passwordValue !== requiredPassword) {
-          event.preventDefault(); // Prevent form submission
           alert('Incorrect password bb gurl. Please try again.');
       }
+      window.location.href = 'sillygoose.html';
   });
 });
